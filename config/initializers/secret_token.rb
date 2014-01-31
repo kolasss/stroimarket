6ee3ba8f45eb0fd0c0f82cca85836296e80a9fa5
kolasss <kolas.batman@gmail.com>
2014-01-31 19:49:32 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stroimarket::Application.config.secret_key_base = 'ebba8a321eafe25a23489fc87f6dc6a778d1b3a9f8c0e4f9ca59f716076e13c3d352b8e6e57d2e930552f138433795a40019749d8d019c91fc12fcb672ed9750'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stroimarket::Application.config.session_store :cookie_store, key: '_stroimarket_session'
